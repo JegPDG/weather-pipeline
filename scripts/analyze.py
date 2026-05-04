ee643ac9800a38dd4e1bdeb7f6d807c2e4e93d0e
Jeg Paduga <145212230+JegPDG@users.noreply.github.com>
2026-05-04 21:26:59 +0800

    started on analyzing

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -10,13 +10,17 @@ df = pd.read_csv(csv_path_clean)
 df["timestamp"] = pd.to_datetime(df["timestamp"])
 df["extracted_at"] = pd.to_datetime(df["extracted_at"])
 
-# Create a new clumn for day and hour
-df['hour'] = df['timestamp'].dt.hour
-df['day'] = df['timestamp'].dt.date
+# Create a new column for day and hour
+df['hour'] = df['timestamp'].dt.floor('h')
+df['day'] = df['timestamp'].dt.floor('D')
 
+# Group by day
+group_by_day = df.groupby('day').mean(numeric_only=True)
 
+print(f"""
+  Table 1: Group by day with an average temperature per day
+  {group_by_day}""")
 
-
-print(df)
 print(df.info())
+print(df.head())
 
