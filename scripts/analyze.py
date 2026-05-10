02b9e14ef215952936071b2bb185713e45f61562
Jeg Paduga <145212230+JegPDG@users.noreply.github.com>
2026-05-10 11:52:44 +0800

    Fixed and modified the analyze.py to get a clean csv file for visualization

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -16,67 +16,80 @@ df['hour'] = df['timestamp'].dt.floor('h')
 df['day'] = df['timestamp'].dt.floor('D')
 
 
-# ///////////////////     GROUPED BY DAY ///////////////
-
+# ///////////////////     GROUPED BY DAY      ///////////////
 
 # Group by day
-group_by_day = df.groupby('day').mean(numeric_only=True).sort_index(ascending=False)
+group_by_day = df.groupby('day').agg(
+  avg_temp=('temperature_c', 'mean'),
+  min_temp=('temperature_c', 'min'),
+  max_temp=('temperature_c', 'max')
+).sort_index(ascending=False)
+
 
 # Getting today date
 today = pd.Timestamp.today().normalize()
 
-# Group by day in the recently
-avg_temp_per_day = group_by_day[group_by_day.index <= today]
+#  Group by day in the recently
+recent_days = group_by_day[group_by_day.index <= today]
 
 # Get the difference from the previous data 
-avg_temp_per_day['yesterday_temp'] = avg_temp_per_day['temperature_c'].shift(-1)
-
-avg_temp_per_day['temp_difference'] = avg_temp_per_day['temperature_c'] - avg_temp_per_day['yesterday_temp']
-
+recent_days['temp_diff_frm_day_before'] = recent_days['avg_temp'] - recent_days['avg_temp'].shift(-1)
 
 # Last 7 days cutoff
-last_7_days = today - timedelta(days=7)
+last_7_days = today - timedelta(days=6)
 
 # Filter only last 7 days and not future dates
-recent_7_days = group_by_day[
-    (group_by_day.index <= today) &
-    (group_by_day.index >= last_7_days)
+recent_7_days = recent_days[
+    (recent_days.index <= today) &
+    (recent_days.index >= last_7_days)
 ]
 
-average_temp = recent_7_days['temperature_c'].mean()
-hottest_day = recent_7_days['temperature_c'].idxmax()
-hottest_temp = recent_7_days['temperature_c'].max()
-coldest_day = recent_7_days['temperature_c'].idxmin()
-coldest_temp = recent_7_days['temperature_c'].min()
+print(recent_7_days)
 
-# //////////////////////// GROUPED BY HOUR ///////////////////////
+# average_temp = recent_7_days['temperature_c'].mean()
+# hottest_day = recent_7_days['temperature_c'].idxmax()
+# hottest_temp = recent_7_days['temperature_c'].max().round(2)
+# coldest_day = recent_7_days['temperature_c'].idxmin()
+# coldest_temp = recent_7_days['temperature_c'].min().round(2)
 
-# Group by hour 
-group_by_hour = df.groupby('hour').mean(numeric_only=True).sort_index(ascending=False)
+# # //////////////////////// GROUPED BY HOUR ///////////////////////
 
-# Last 7 days cutoff 
-last_7_days_by_hour = group_by_hour[(group_by_hour.index <= today) & (group_by_hour.index >= last_7_days)]
+# # Group by hour 
+df['hour'] = df['hour'].dt.hour
+
+group_by_hour = df.groupby('hour').agg(
+  avg_temp=('temperature_c', 'mean'),
+  min_temp=('temperature_c', 'min'),
+  max_temp=('temperature_c', 'max'),
+
+).round(2)
+
+print(group_by_hour)
+
+# # Hottest hour of the day 
+# hottest_temp_by_hour = group_by_hour['temperature_c'].max().round(2)
+# hottest_hour = group_by_hour['temperature_c'].idxmax()
+# coldest_temp_by_hour = group_by_hour['temperature_c'].min().round(2)
+# coldest_hour = group_by_hour['temperature_c'].idxmin()
 
 
-print(f"""
-  Table 1: The average temperature per day in Aklan
-  {avg_temp_per_day}
+# print(f"""
+#   Table 1: The average temperature per day in Aklan
+#   {avg_temp_per_day}
 
-  Q: Is it getting hotter or colder here in Aklan
-    Data shows that the temparature difference from the day before is
-    showing a positive value, which means that the temperature the next day is getting higher.
-    The data shows that the temperature is getting hotter day by day.
+#   Average temperature in the last 7 days: {average_temp}
+#   Hottest day in the last 7 days: {hottest_day} with a temperature of {hottest_temp}
+#   Coldest day in the last 7 days: {coldest_day} with a temperature of {coldest_temp}
 
-  Average temperature in the last 7 days: {average_temp}
-  Hottest day in the last 7 days: {hottest_day} with a temperature of {hottest_temp}
-  Coldest day in the last 7 days: {coldest_day} with a temperature of {coldest_temp}
+#   """)
 
-  """)
+# print(f""" 
+#   Table 2: Group by hour
+  
 
-print(f""" 
-  Table 2: Group by Hour in the
-  {last_7_days_by_hour}
+#   Hottest hour of the day: {hottest_temp_by_hour} at hour {hottest_hour}:00
+#   Coldest hour of the day: {coldest_temp_by_hour} at hour {coldest_hour}:00
 
-""")
+# """)
 
 
